9ca45164b45704bd2a3902c9e04713f5b65a7996
LouayKliai <kliailouay@gmail.com>
2023-11-09 22:35:28 +0100

    Initial commit

---
+++ src/main/java/com/demo/ECommerceLouayApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceLouayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceLouayApplication.class, args);
+	}
+
+}
